e8b81c8e55774fd6082d43836e2f7430c848cf28
UbaydaAbdulsamad <ubaydasmd@gmail.com>
2022-05-25 19:14:48 +0300

    loading photos is working

--- APP.py
+++ APP.py
@@ -1,22 +1,105 @@
-import PIL.Image
+import sys
+import os
+import numpy as np
+from NoiseRestoration.filters import noise_gaussian, median_filter
+from PyQt5.QtWidgets import QMainWindow, QStatusBar, QApplication, QLabel, QRadioButton, QListWidget, QListWidgetItem, QStatusBar, QVBoxLayout
+from PyQt5 import QtGui, uic
+from PyQt5.QtGui import QImage, QPixmap
 import cv2 as cv
-from NoiseRestoration.filters import *
 
-image = cv.imread("nature.jpeg")
-image = cv.resize(image, (800, 600), interpolation=cv.INTER_LINEAR)
-cv.imshow('image', image)
+def cv2qim(image):
+    height, width, channel = image.shape
+    bytesPerLine = 3 * width
+    q_image = QImage(image.data, width, height, bytesPerLine, QImage.Format_BGR888)
+    return q_image
 
-noisy = noise_salt_pepper(image, 0.01)
-noisy = cv.resize(noisy, (800, 600), interpolation=cv.INTER_LINEAR)
-cv.imshow('noisy', noisy)
-cv.imwrite("noisy.jpeg", noisy)
-im = Image.open('/Users/hazemkilzieh/PycharmProjects/DIP_Restoration_Reconstruction_Compression/gaussian_noise_img.jpg')
+class NoiseRestorationAPP(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('APP.ui', self)
 
-box_blur_kernel = np.reshape(np.ones(3*3),(3,3)) / (3*3)
-im1 = im.filter(ImageFilter.Kernel((3,3), box_blur_kernel.flatten()))
-im.show()
-im1.show()
-cv.waitKey(0)
 
-def arithmatic_mean_filter(image, kernel_size):
-    return None
+        # main variables
+        self.current_item = QListWidgetItem
+        self.img_width = int(app.primaryScreen().size().width()/2 - 200)
+        self.img_height = int(app.primaryScreen().size().height()/2 - 20)
+
+        # defining widgets
+        self.images_list = self.findChild(QListWidget, 'images_list')
+        self.original_label = self.findChild(QLabel, 'original_label')
+        self.noisy_label = self.findChild(QLabel, 'noisy_label')
+        self.restored_label = self.findChild(QLabel, 'restored_label')
+        self.images_layout = self.findChild(QVBoxLayout, 'images_layout')
+
+        # connecting signals
+        self.images_list.itemClicked.connect(self.update_original)
+
+        # initial functions
+        self.populate_list("images\\")
+
+    def update_original(self):
+        item = self.images_list.currentItem()
+
+        q_image = item.data(5)
+        if q_image.height() > self.img_height:
+            q_image = q_image.scaledToHeight(self.img_height)
+
+        self.original_label.setPixmap(QPixmap(q_image))
+        self.update_noisy()
+
+    def update_noisy(self):
+        original = self.images_list.currentItem().data(6)
+        noisy = noise_gaussian(original, 5, 50)
+        self.images_list.currentItem().setData(7, noisy)
+
+        q_image = cv2qim(noisy)
+        if q_image.width() > self.img_width:
+            q_image = q_image.scaledToWidth(self.img_width)
+        if q_image.height() > self.img_height:
+            q_image = q_image.scaledToHeight(self.img_height)
+
+        self.noisy_label.setPixmap(QPixmap(q_image))
+        self.update_restored()
+
+    def update_restored(self):
+        noisy = self.images_list.currentItem().data(7)
+        restored = median_filter(noisy, 5)
+        self.images_list.currentItem().setData(8, restored)
+
+        q_image = cv2qim(restored)
+        if q_image.width() > self.img_width:
+            q_image = q_image.scaledToWidth(self.img_width)
+        if q_image.height() > self.img_height:
+            q_image = q_image.scaledToHeight(self.img_height)
+
+        self.restored_label.setPixmap(QPixmap(q_image))
+
+
+    def populate_list(self, folder_path):
+        self.images_list.clear()
+        images = os.listdir(folder_path)
+
+        for image in images:
+            path = os.path.join(folder_path, image)
+
+            icon = QtGui.QIcon()
+            icon.addPixmap(QPixmap(path), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+
+            item = QListWidgetItem()
+            item.setIcon(icon)
+            item.setData(0, QPixmap(path))
+            item.setData(5, QImage(path))
+            # Original, Noisy, Restored (6, 7, 8)
+            item.setData(6, cv.imread(path))
+            self.images_list.addItem(item)
+
+        self.images_list.setCurrentItem(self.images_list.item(0))
+        self.update_original()
+        self.showMaximized()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    demo = NoiseRestorationAPP()
+    demo.show()
+    sys.exit(app.exec_())
